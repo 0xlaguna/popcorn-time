c3e61c3de1f5276e74e4d2f8baa4204a4e68e398
Maykol Laguna <lagunamichael2@outlook.com>
2022-04-18 21:41:08 -0600

    feat(api): get all movies

---
+++ popcorn-time-api/popcorn_api/movies/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+
+from .models import Movie
+
+
+class MovieSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Movie
+        fields = (
+            "id",
+            "title",
+            "year",
+            "runtime",
+            "released",
+            "genre",
+            "cover",
+        )
--- popcorn-time-api/popcorn_api/movies/views.py
+++ popcorn-time-api/popcorn_api/movies/views.py
@@ -1,1 +1,9 @@
-# Create your views here.
+from rest_framework import viewsets
+
+from .models import Movie
+from .serializers import MovieSerializer
+
+
+class MovieView(viewsets.ModelViewSet):
+    serializer_class = MovieSerializer
+    queryset = Movie.objects.all()
